44dfbd4e194d3a8ceabfc714bb125c6359b3acc0
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-15 10:38:10 -0600

    login and register endpoints now working, recieving token on login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,13 +2,49 @@ const router = require('express').Router();
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 
+const Users = require("../users/users-model.js");
+
 
 router.post('/register', (req, res) => {
   // implement registration
+  const user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      console.log("registration server err", err)
+      res.status(500).json(err)
+    })
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwtToken(user.username);
+
+        res.status(200).json({
+          message: `welcome ${user.username}`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "invalid credentials"})
+      }
+    })
+    .catch(err => {
+      console.log("login error", err);
+      res.status(500).json(err);
+    });
+
 });
 
 function getJwtToken(username) {
